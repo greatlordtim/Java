3ad0ce8b5fee27840df0789fa5df2923f12d6f0c
Jimmy Liu <sicong.liu98@gmail.com>
2013-01-25 13:59:22 -0800

    New JFrame

--- Calvin/Calvin.java
+++ Calvin/Calvin.java
@@ -12,6 +12,19 @@ public class Calvin {
 
 
 
+public void JFrame {
+	JFrame frame = new JFrame("Calvin");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	panel = new MyPanel();
+	frame.getContentPane().add(panel, BorderLayout.CENTER);
+	frame.setSize(500, 500);
+	frame.setLocation(200, 300);
+	frame.setVisible(true);
+}
+
+
+
+
 
 
 
