35ede49ee0f9bf77b3104b71e1c4df87f20c59ff
root <root@website.vortexunit.com>
2013-02-12 08:58:34 +0300

    Added draw

---
+++ Draw/Web.java
@@ -0,0 +1,15 @@
+//Java Files
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Calvin {
+
+    JFrame frame; //Creates new Jframe and JPanel
+    MyPanel panel;
+
+    public static void main(String[] args) {
+        Calvin calvin = new Calvin();
+        calvin.Calvin(); //Constructers
+    }
