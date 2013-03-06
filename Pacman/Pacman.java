f3dc667e705df157662ce12ebf24f1c81ce27175
Jimmy Liu <sicong.liu98@gmail.com>
2013-03-06 01:33:49 -0800

    HAH

--- Pacman/Pacman.java
+++ Pacman/Pacman.java
@@ -6,6 +6,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class Pacman {
 	
@@ -13,6 +14,8 @@ public class Pacman {
 	DerpPanel np;			// KeyListener, FocusListener panel with text field
 	GridPanel gp;
 	int[][] array = new int[10][10];
+	int move = 0;
+	int xloc, yloc; //pacman x and y
 	private JLayeredPane lpane = new JLayeredPane();
 
 	
@@ -42,7 +45,7 @@ public class Pacman {
 
 		np = new DerpPanel();
 		
-		//stand back, I'm going to try this
+		//stand back, I'm doing science
         frame.setSize(new Dimension(500, 560));
         frame.setLayout(new BorderLayout());
         frame.add(lpane, BorderLayout.CENTER);
@@ -109,6 +112,56 @@ public class Pacman {
    			g.setColor(Color.black);
    			g.setFont(new Font("Helvetica", Font.BOLD, 20));
    			g.drawString("Directions: Ohai", 20, 530);
+   			//end red bottom
+
+   			if (move==0) { //first turn pacman generate
+   				xloc = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   				yloc = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   				array[xloc][yloc] = 4;
+
+   				for (int z = 0; z < 6; z++) {
+   					int cheesex = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   					int cheesey = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   					array[cheesex][cheesey] = 3;
+				} //end while
+
+				for (int z = 0; z < 6; z++) {
+   					int ghostx = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   					int ghosty = 0 + (int)(Math.random() * ((9 - 0) + 1));
+   					array[ghostx][ghosty] = 2;
+				} //end while
+
+   				move++;
+   				
+   			} //end pacman
+
+   			
+
+
+
+
+   			//printing array
+   			for(int i=0; i < 10; i++) {
+				for(int j=0; j < 10; j++) {
+					int a = array[i][j];
+
+					if (a == 4) { //pacman
+						g.setColor(Color.black);
+						g.fillRect((i+1) * 48, (j+1) * 48, 5, 5);
+					}
+
+					if (a == 3) { //ghost
+						g.setColor(Color.blue);
+						g.fillRect((i+1) * 48, (j+1) * 48, 5, 5);
+					}
+
+					if (a == 2) { //cheese
+						g.setColor(Color.yellow);
+						g.fillRect((i+1) * 48, (j+1) * 48, 5, 5);
+					}
+
+				}
+			}
 
 
 			/*if(move == 0) //if first move, draw square in the center
@@ -152,19 +205,29 @@ public class Pacman {
 			System.out.println("keyPressed, c= " + c);
 			switch (c) { //Switch case for getting what direction the user wants to go
 
-				case 'a': 
+				case 'a':
+				array[xloc-1][yloc] = 4;
+				array[xloc][yloc]=1;
+				xloc = xloc-1;
+				repaint();
 				
 				break;
 
 				case 'd':
+
+				repaint();
 				
 				break;
 
 				case 'w':
+
+				repaint();
 				
 				break;
 
 				case 's':
+
+				repaint();
 				
 				break;
 
