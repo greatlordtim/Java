f0e85775bd073184fe7558a57b7d91049addc3ae
Jimmy Liu <sicong.liu98@gmail.com>
2013-03-25 13:14:09 -0700

    Yolo?

--- Indian/yolo/Test.java
+++ Indian/yolo/Test.java
@@ -1,24 +1,9 @@
-public  class Test{
-	public static void main(String []args){
-		String phrase = "My name is Anindit";
-		int x = 0;
-		int spaces = 0;
-		int a = 0;
-		boolean b = true;
-		
-		phrase = remove(phrase, "it");
-		System.out.println(phrase);
-	}
-	public static String remove(String s, String f){
-		String d = "";
-		System.out.println(f.length());
-		if (s.endsWith(f)){
-			for (int i = 0; i < (s.length() - f.length()); i++){
-				d += s.charAt(i);
-				System.out.println(d);
-			}
-		}	
-		
-		return d;
+public class test{
+	public static void main(String[] args){
+		int b = 10000;
+		for (int i = 5600; i > 25; i += b){
+			System.out.println(i);
+		}
+	
 	}
 }
